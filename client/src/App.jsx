c68fac8875fec450b378fe39a98b83949c153a51
voisnoa <markzlopp@gmail.com>
2025-02-15 11:54:30 +0530

    added button and react

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="reaction-time-tester">
+        <Button />
+      </div>
+    </div>
+  );
+}
+
+export default App;
