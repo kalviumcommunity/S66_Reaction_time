abdf0e163c3f1f526e66fe79adb3d992518fe8e9
voisnoa <markzlopp@gmail.com>
2025-03-12 10:45:08 +0530

    cloudfalre website live

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddEntity from "./components/AddEntity";
+import FormPage from "./components/AddForm"
 import Home from "./Home";
+import Leaderboard from "./components/Form";
 import "./app.css";
 
 function App() {
@@ -9,11 +11,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/add-entity" element={<AddEntity />} />
+          <Route path="/add-entity" element={<AddEntity />} />  
+          <Route path="/form" element={<FormPage/>}/>
+          <Route path="/leaderboard" element={<Leaderboard/>}/>
         </Routes>   
       </BrowserRouter>
     </>
   );
 }
 
-export default App;
+export default App; 
